9c46f1cef5a9087c38d86071df2ec68bc3930321
Markus Binsteiner <markus@frkl.io>
2022-08-19 12:55:09 +0200

    chore: proof-of-concept value view

---
+++ src/lib/ValueRender.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+
+    import ValueSelect from './ValueSelect.svelte';
+    import ValueView from './ValueView.svelte';
+
+	import { onMount } from 'svelte';
+
+    let all_aliases: string[] = []
+    var selected_value: string = undefined
+
+    let render_result: object = {
+        "rendered": undefined,
+        "metadata": {
+            "related_scenes": undefined
+        }
+    }
+
+    async function retrieve_value_names() {
+        console.log("retrieve value names")
+        const res = await fetch('http://127.0.0.1:8080/data/alias_names')
+        all_aliases = await res.json()
+        console.log("aliases", all_aliases)
+    }
+
+	async function retrieve_render_result (render_config) {
+        // const value_id = "391ef603-46c0-4e22-9672-ff9c3527e44a"
+        if (selected_value != undefined) {
+            const res = await fetch('http://127.0.0.1:8080/data/render/' + selected_value, {
+                method: 'POST',
+                body: JSON.stringify(render_config)
+            })
+
+            render_result = await res.json()
+        }
+	}
+    async function handle_new_value(event) {
+
+        const old_selected = selected_value
+        console.log("New value", event.detail)
+        selected_value = event.detail
+        if (old_selected != selected_value) {
+            retrieve_render_result({})
+        }
+
+    }
+
+    async function handle_new_render_scene(render_config) {
+        console.log("NEW SCENE")
+        console.log(render_config.detail)
+        await retrieve_render_result(render_config.detail.render_config)
+    }
+	onMount(async () => {
+        await retrieve_value_names()
+	});
+</script>
+
+<ValueSelect value_names={all_aliases} on:selected_value={handle_new_value}></ValueSelect>
+<ValueView render_result={render_result} on:new_render_scene={handle_new_render_scene} >
+
+</ValueView>
---
+++ src/lib/ValueSelect.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Select from 'svelte-select';
+
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    export let value_names: string[] = []
+    export let selected = undefined
+
+    function handleSelect(event) {
+        console.log("SELECTED", event)
+        dispatch('selected_value', event.detail.value);
+    }
+
+</script>
+
+<span>
+<Select items={value_names} value={selected} on:select={handleSelect}></Select>
+</span>
